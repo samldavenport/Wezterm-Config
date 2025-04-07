550bf6c5c8ce6b64e861a17dca38e082670b6b94
thereclaimer <91574297+davdronographx@users.noreply.github.com>
2025-04-07 14:05:19 -0500

    updated the window border

--- wezterm.lua
+++ wezterm.lua
@@ -22,12 +22,7 @@ config.colors = {
 	cursor_bg = "#b8bb26",
 }
 
---config.window_decorations = "RESIZE"
-
-config.enable_tab_bar = false
-config.use_fancy_tab_bar = false
-config.show_tabs_in_tab_bar = false
-config.show_new_tab_button_in_tab_bar = false
+config.window_decorations = "RESIZE"
 
 -- and finally, return the configuration to wezterm
 return config
