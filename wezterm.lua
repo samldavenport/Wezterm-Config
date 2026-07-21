5dfcac4318f3d33d081ddbdf15ba7bb04f26facb
Sam L Davenport <157653319+samldavenport@users.noreply.github.com>
2026-07-21 11:48:06 -0500

    Update wezterm.lua

--- wezterm.lua
+++ wezterm.lua
@@ -1,28 +1,31 @@
 -- Pull in the wezterm API
 local wezterm = require("wezterm")
 
+font_antialias = 'Greyscale' 
+
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
-config.font = wezterm.font("FiraCode Nerd Font")
-config.default_cwd = ".\\source\\repos"
-config.default_prog = { "powershell.exe", "-nologo" }
+config.window_decorations = "RESIZE"
+config.font               = wezterm.font("SauceCodePro Nerd Font Mono",{weight="Regular", stretch='Normal', style=Normal})
+config.font_size          = 12.0 
+config.default_cwd        = ".\\source\\repos"
+config.default_prog       = { "powershell.exe", "-nologo" }
 
 --cursor
-config.default_cursor_style = "BlinkingBlock"
-config.cursor_blink_rate = 500
+config.default_cursor_style  = "BlinkingBlock"
+config.cursor_blink_rate     = 500
 config.cursor_blink_ease_out = "Constant"
-config.cursor_blink_ease_in = "Constant"
+config.cursor_blink_ease_in  = "Constant"
 
 --colors
 config.color_scheme = "Gruvbox Material (Gogh)"
-config.colors = {
+config.colors       = {
 
 	background = "#202020",
-	cursor_bg = "#b8bb26",
+	cursor_bg = "#b1fc03",
 }
 
-config.window_decorations = "RESIZE"
 
 -- and finally, return the configuration to wezterm
 return config
