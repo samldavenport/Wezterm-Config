31645c6acf6cff43bac591974bd9036419aeaee7
thereclaimer <sam.davenport@thereclaimergame.com>
2026-07-24 12:03:39 -0500

    opacity and image alignment

--- wezterm.lua
+++ wezterm.lua
@@ -3,12 +3,17 @@ local wezterm                = require("wezterm")
 
 font_antialias               = 'Greyscale'
 
--- This will hold the configuration.
 local config                 = wezterm.config_builder()
 
 config.window_decorations    = "RESIZE"
-config.font                  = wezterm.font("SauceCodePro Nerd Font Mono",
-    { weight = "Regular", stretch = 'Normal', style = Normal })
+config.font                  = wezterm.font(
+    "SauceCodePro Nerd Font Mono",
+    {
+        weight  = "Regular",
+        stretch = 'Normal',
+        style   = Normal
+    }
+)
 config.font_size             = 12.0
 config.default_cwd           = ".\\source\\repos"
 config.default_prog          = { "powershell.exe", "-nologo" }
@@ -37,29 +42,33 @@ end
 
 config.window_background_opacity = 1.0
 
-config.background = {
-  -- LAYER 1 (Bottom): The solid base color to block the desktop bleed
-  {
-    source = {
-      Color = '#202020', -- Put your preferred background hex color here
+config.background                = {
+    -- LAYER 1 (Bottom): The solid base color to block the desktop bleed
+    {
+        source           = {
+            Color = '#1D2021', -- Put your preferred background hex color here
+        },
+        width            = '100%',
+        height           = '100%',
+        opacity          = 1.0, 
+        horizontal_align = "Center",
+        vertical_align   = "Middle",
+        repeat_x         = "NoRepeat",
+        repeat_y         = "NoRepeat",
     },
-    width = '100%',
-    height = '100%',
-    opacity = 1.0     -- Must be 1.0 to stay solid
-  },
 
     -- Random image over the solid color
     {
         source = {
             File = images[math.random(#images)],
         },
-        opacity = 0.05,
+        opacity = 0.1
     }
 }
 
 --colors
-config.color_scheme = "Gruvbox Material (Gogh)"
-config.colors       = {
+config.color_scheme              = "Gruvbox Material (Gogh)"
+config.colors                    = {
 
     cursor_bg = "#b1fc03",
 }
